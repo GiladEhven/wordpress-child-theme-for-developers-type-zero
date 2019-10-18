7dc02f975696a29e3632f88a229192c7068ddb3b
Gilad Ehven <gilad@ehven.com>
2019-10-18 07:09:10 -0400

    Added sample 'editor-color-palette' with 'disable-custom-colors'.

--- functions/admin.php
+++ functions/admin.php
@@ -5,3 +5,90 @@
     if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+    add_theme_support( 'disable-custom-colors' );
+
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name'  => __( 'Black', 'type-zero-child' ),
+            'slug'  => 'black',
+            'color' => '#000',
+        ),
+        array(
+            'name'  => __( 'Gray 1', 'type-zero-child' ),
+            'slug'  => 'gray-one',
+            'color' => '#111',
+        ),
+        array(
+            'name'  => __( 'Gray 2', 'type-zero-child' ),
+            'slug'  => 'gray-two',
+            'color' => '#222',
+        ),
+        array(
+            'name'  => __( 'Gray 3', 'type-zero-child' ),
+            'slug'  => 'gray-three',
+            'color' => '#333',
+        ),
+        array(
+            'name'  => __( 'Gray 4', 'type-zero-child' ),
+            'slug'  => 'gray-four',
+            'color' => '#444',
+        ),
+        array(
+            'name'  => __( 'Gray 5', 'type-zero-child' ),
+            'slug'  => 'gray-five',
+            'color' => '#555',
+        ),
+        array(
+            'name'  => __( 'Gray 6', 'type-zero-child' ),
+            'slug'  => 'gray-six',
+            'color' => '#666',
+        ),
+        array(
+            'name'  => __( 'Gray 7', 'type-zero-child' ),
+            'slug'  => 'gray-seven',
+            'color' => '#777',
+        ),
+        array(
+            'name'  => __( 'Gray 8', 'type-zero-child' ),
+            'slug'  => 'gray-eight',
+            'color' => '#888',
+        ),
+        array(
+            'name'  => __( 'Gray 9', 'type-zero-child' ),
+            'slug'  => 'gray-nine',
+            'color' => '#999',
+        ),
+        array(
+            'name'  => __( 'Gray A', 'type-zero-child' ),
+            'slug'  => 'gray-a',
+            'color' => '#aaa',
+        ),
+        array(
+            'name'  => __( 'Gray B', 'type-zero-child' ),
+            'slug'  => 'gray-b',
+            'color' => '#bbb',
+        ),
+        array(
+            'name'  => __( 'Gray C', 'type-zero-child' ),
+            'slug'  => 'gray-c',
+            'color' => '#ccc',
+        ),
+        array(
+            'name'  => __( 'Gray D', 'type-zero-child' ),
+            'slug'  => 'gray-d',
+            'color' => '#ddd',
+        ),
+        array(
+            'name'  => __( 'Gray E', 'type-zero-child' ),
+            'slug'  => 'gray-e',
+            'color' => '#eee',
+        ),
+        array(
+            'name'  => __( 'White', 'type-zero-child' ),
+            'slug'  => 'white',
+            'color' => '#fff',
+        ),
+    ));
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
