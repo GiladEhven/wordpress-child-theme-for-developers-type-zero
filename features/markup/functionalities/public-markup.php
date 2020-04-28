ef89218e847e8bd78d22de589c6e39b8116e999f
Gilad Ehven <gilad@ehven.com>
2020-04-28 00:00:55 -0400

    Added 3 'remove_action' calls to further clean up public markup.

--- features/markup/functionalities/public-markup.php
+++ features/markup/functionalities/public-markup.php
@@ -6,7 +6,13 @@
 
     //  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  //
 
+	remove_action( 'wp_head', 'adjacent_posts_rel_link' );
+
+	remove_action( 'wp_head', 'index_rel_link' );
+
 	remove_action( 'wp_head', 'rsd_link' );
+
+	remove_action( 'wp_head', 'start_post_rel_link' );
 
 	remove_action( 'wp_head', 'wlwmanifest_link' );
 
